4c1d84149ce4c25de578f9f23f4cccdae64316ee
Ahmad Raza <raza88581@gmail.com>
2021-10-28 15:22:35 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [files, setFiles] = useState([]);
+
+  const handleChange = (e) => {
+    let value = e.target.files[0];
+    //console.log(value);
+    setFile(value);
+  };
+  const postData = async (e) => {
+    e.preventDefault();
+    console.log(file);
+    const data = new FormData();
+    data.append("file", file);
+    await axios
+      .post("http://localhost:8000/single", data)
+      .then((res) => {
+        console.log("Response: ", res);
+      })
+      .catch((err) => console.log("Error: ", err.message));
+  };
+
+  const handleMultipleChange = (e) => {
+    let value = e.target.files;
+    setFiles(value);
+  };
+
+  const postMultipleData = async (e) => {
+    e.preventDefault();
+    console.log(files);
+    const data = new FormData();
+    for (let x = 0; x < files.length; x++) {
+      data.append("mFile", files[x]);
+    }
+    await axios
+      .post("http://localhost:8000/multiple", data)
+      .then((res) => {
+        console.log("Response: ", res);
+      })
+      .catch((err) => console.log("Error: ", err.message));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="create">
+      <h2>File Upload</h2>
+      <form method="POST">
+        <label>Single file upload</label>
+        <input
+          type="file"
+          name="file"
+          accept="image/*, application/pdf"
+          onChange={handleChange}
+        />
+        <button type="submit" onClick={postData}>
+          Submit
+        </button>
+      </form>
+      <form method="POST">
+        <label>Multiple file upload</label>
+        <input
+          type="file"
+          name="mFile"
+          multiple
+          accept="image/*, application/pdf"
+          onChange={handleMultipleChange}
+        />
+        <button type="submit" onClick={postMultipleData}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
